e1ef73cfb34c204d764002a21087169cff895052
ianvizarra <ian.vizarra@gmail.com>
2020-04-08 23:32:32 +0800

    Fixed Unit Testcase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,10 @@ namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
+if (!defined('LARAVEL_START')) {
+    define('LARAVEL_START', microtime(true));
+}
+
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
